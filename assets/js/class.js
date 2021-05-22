11d012692879d8ddccc931f7a930e2edf905ef67
Princess <sonali.patel.pkg@gmail.com>
2021-05-22 14:54:00 -0500

    initial commit and pseudo code

---
+++ assets/js/class.js
@@ -0,0 +1,20 @@
+//global variables
+// search history as array
+// api URL
+// api key
+
+
+// ref DOM elements
+
+// function to display search history from user input
+var cardHead = document.createElement('div')
+
+    // allow access to city name when clicked
+
+// function to display current weather data & append data to page
+
+// create second API to retrieve the 5 day weather forecast 
+
+// function to get UV index and render to page
+
+// create handler to create search feild and return data, if no data just return
