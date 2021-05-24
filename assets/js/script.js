bfcf3a4b47cd034d14e3875c8f77c6683b585e97
Princess <sonali.patel.pkg@gmail.com>
2021-05-24 12:38:36 -0400

    add basic script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,28 @@
 // search movie input
-var searchMovieEl = document.getElementById('movie');
+var searchLocEl = document.getElementById('location-input');
 
 // find results
-var movieTitleEl = document.getElementById('title');
-var movieGenreEl = document.getElementById('genre');
-var moviePlotEl = document.getElementById('plot');
-var movieRatingEl = document.getElementById('ratings')
+var movieCurrentEl = document.getElementById('current-weather');
 
-function apiMovie() {
+
+function apiCurrentWeather() {
     
     // variable for movie search
-    var userSearch= searchMovieEl.value;
+    var userSearch= searchLocEl.value;
     console.log(userSearch);
     
     // fetch API
-    $.getJSON('https://www.omdbapi.com/?t=' + userSearch + '&apikey=7a94102a')
+    $.getJSON('api.openweathermap.org/data/2.5/weather?q=' + userSearch + '&&appid=065c88a61393c9d19ed23b6fbcfd7819')
     .then(function(response){
         console.log(response);
+
+        var movieTitle= document.createElement('p')
+        movieTitle.textContent=response.Title
+        movieTitleEl.appendChild(movieTitle)
     
-    })
+    });
 }
+
+apiCurrentWeather();
+
+// document.getElementById('location-button').addEventListener('click', apiCurrentWeather);
